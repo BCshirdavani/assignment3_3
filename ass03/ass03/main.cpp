70d88686f1b3399935ad88f7438a47ad543f08ba
Beau Shirdavani <beau.shirdavani@bellevuecollge.edu>
2017-11-25 11:31:43 -0800

    ass 01 - testing string functions
    string has class member functions:
    lenght()
    and i can access each element with  [ ]
    will use this in for loop to push to stacks.

---
+++ ass03/ass03/main.cpp
@@ -0,0 +1,30 @@
+//****************************************************
+//
+//    Author:       Beau Shirdavani
+//    Date:         11-23-2017
+//    Title:        Assignment 3_3
+//                  main.cpp
+//
+//      main theory of this math will be to add numbers of
+//      matching columns from each stack, and multiply them
+//      by the appropriate power of 10 each, then continue to
+//      add next column
+//
+//****************************************************
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    string s1 = "thisString";
+    cout << "length of string: "  << s1.length() << endl;
+    cout << "elements 0 - 2 fo string: " << s1[0] << endl;
+    
+    // print string as loop
+    for ( int i = 0; i < s1.length(); i++ ){
+        cout << s1[i] << endl;
+    }
+    
+    
+    return 0;
+}

1acd1e417d28c36669514c9b6a522012f04d41c6
Beau Shirdavani <beau.shirdavani@bellevuecollge.edu>
2017-11-26 15:54:54 -0800

    math works, but one problem still exists
    program truncates some digits from final sum.

--- ass03/ass03/main.cpp
+++ ass03/ass03/main.cpp
@@ -10,59 +10,81 @@
 //      by the appropriate power of 10 each, then continue to
 //      add next column
 //
+//      ERROR:      This somewho keeps truncating digits off the sum
+//                  the first loop makes the correct number,
+//                  but the second loop loses a few digits in the sum result
+//
 //****************************************************
 
 #include <iostream>
+#include <math.h>
 #include "LinkedStack.h"
 using namespace std;
 
 int main() {
-    string s1 = "thisString";
-    cout << "length of string: "  << s1.length() << endl;
-    cout << "elements 0 - 2 fo string: " << s1[0] << endl;
+
+    //****************************************************
+    //                                      read in numbers
+    string num1;
+    string num2;
+    LinkedStack<float>* s1;
+    s1 = new LinkedStack<float>();
+    LinkedStack<float>* s2;
+    s2 = new LinkedStack<float>();
+    LinkedStack<float>* s3;
+    s3 = new LinkedStack<float>();
     
-    // print string as loop
-    for ( int i = 0; i < s1.length(); i++ ){
-        cout << s1[i] << endl;
+    cout << "enter a very large number: ";
+    getline(cin, num1);
+    cout << "enter a very large number: ";
+    getline(cin, num2);
+
+    //****************************************************
+    //                              make stacks from string
+    float temp;
+    for ( int i=0; i < num1.length(); i++ ){
+        temp = (num1[i] - '0');
+        s1->push(temp);
     }
+    for ( int i=0; i < num2.length(); i++ ){
+        temp = (num2[i] - '0');
+        s2->push(temp);
+    }
+    // test of stack view and number conversion
+    cout << "num1 = " << num1 << endl;
+    cout << "s1 = " << *s1 << endl;\
+    cout << "s1.length() = " << s1->getLength() << endl;
+    cout << "num2 = " << num2 << endl;
+    cout << "s2 = " << *s2 << endl;
+    cout << "s2.length() = " << s2->getLength() << endl;
     
+    //****************************************************
+    //                                      do adding math
+    //      add a pop() number from stack to the sum
+    //      after multiplying by the correct power of 10
+    float sum = 0;
+    float tempSum_s1 = 0;
+    float powerSum_s1 = 0;
+    for ( int i = 0; i <= s1->getLength()+1; i++ ){
+//        cout << "\ti: " << i << endl;
+        tempSum_s1 = s1->pop();
+        powerSum_s1 = tempSum_s1 * ( pow(10,i) );
+        sum = sum + powerSum_s1;
+//        cout << "\t\tsum : " << sum << endl;
+    }
+//    cout << "Sum is part 1 : " << sum << endl;
     
-    //
-    
-    
-    
-    
-    
-    
-    // practice for assignment4_4
-    // reverse an integer
-    // trying to take the last digit off, and record it
-    // using a combo of div by 10 to isolate ending, then modulus 10
-    int x = 1234;
-    cout << "x: " << x << endl;
-    int y;
-    y = x/10;
-    cout << "y = x/10: " << y << endl;
-    int z;
-    z = (y%10);
-    cout << "z = y % 10: " << z << endl;
-    int w = 2%3;
-    cout << w << endl;
-    
-    // that works, but think about infinite recursion...when would recursive call stop?
-    int q;
-    q = y/10;
-    cout << "q = y/10: " << q << endl;
-    int r;
-    r = q/10;
-    cout << "r = q/10: " << r << endl;
-    int t;
-    t = r/10;
-    cout << "t = r/10: " << t << endl;
-    int v;
-    v = t/10;
-    cout << "v = t/10: " << v << endl;
-    // check the length of the itneger maybe?
+    float tempSum_s2 = 0;
+    float powerSum_s2 = 0;
+    for ( int j = 0; j <= s2->getLength()+1; j++ ){
+//        cout << "\tj: " << j << endl;
+        tempSum_s2 = s2->pop();
+        powerSum_s2 = tempSum_s2 * ( pow(10,j) );
+        sum = sum + powerSum_s2;
+//        cout << "\t\tsum : " << sum << endl;
+    }
+
+    cout << "Sum = " << sum << endl;
     
     return 0;
 
